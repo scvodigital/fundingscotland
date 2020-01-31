afe9db7ad0735a40b863ca7d4637e2c4d18a117d
Calum MacUisdean <calum.macuisdean@gmail.com>
2020-01-31 14:36:07 +0000

    added new corporate font and scvo sites bar

--- src/main.js
+++ src/main.js
@@ -53,12 +53,12 @@ export class FundingScotland {
         .map(field => '[name="' + field.name + '"][value="' + field.value + '"]');
 
     // Headroom
-    var header = document.querySelector("header.top-bar-stuck");
-    var headroom  = new Headroom(header, {
-      "offset": 100,
-      "tolerance": 5
-    });
-    headroom.init();
+    // var header = document.querySelector("header.top-bar-stuck");
+    // var headroom  = new Headroom(header, {
+    //   "offset": 100,
+    //   "tolerance": 5
+    // });
+    // headroom.init();
 
     const ci = new cookieinfo();
     ci.options.message = "We use cookies to track anonymous usage statistics and do not collect any personal information that can be used to identify you. By continuing to visit this site you agree to our use of cookies.";
